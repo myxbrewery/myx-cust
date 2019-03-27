daf74655c96ecbbc05dffc0b99daa28915333251
Gabriel Wong <gabrielwong159@gmail.com>
2019-03-27 12:06:04 +0800

    Initial commit with notifications demo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+if (!('Notification' in window)) {
+  alert('This browser does not support desktop notifications.');
+}
+else {
+  Notification.requestPermission()
+    .then(permission => console.log);
+}
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <a class="btn" @click="notify(0)">
+      Notify
+    </a>
+    <a class="btn" @click="notify(3000)">
+      Notify with delay
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  methods: {
+    notify(delay) {
+      let title = 'Title';
+      let body = 'Body';
+      setTimeout(() => new Notification(title, { body }), delay);
+    }
+  },
+};
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
