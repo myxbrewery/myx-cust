77aed1398d0e942bffe2db652f7d50b804bbec76
Gabriel Wong <gabrielwong159@gmail.com>
2019-04-05 01:19:09 +0800

    Add total

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -8,6 +8,13 @@
 			<p class="price">${{ (item.price * item.quantity).toFixed(2) }}</p>
 		</div>
 
+		<div id="total-container">
+			<div class="entry" id="total">
+				<p class="item">Total</p>
+				<p class="price">${{ computeTotal().toFixed(2) }}</p>
+			</div>
+		</div>
+
 		<router-link class="btn waves-effect waves-light"
 			:to="{ name: 'Pay', params: { amount: computeTotal() } }">
 			Pay
@@ -43,10 +50,20 @@ export default {
 </script>
 
 <style scoped>
+#checkout {
+	height: 100%;
+	padding-bottom: 57px;
+
+	display: flex;
+	flex-direction: column;
+}
+
 .entry {
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
+
+	width: 100%;
 }
 
 .entry * {
@@ -69,6 +86,22 @@ export default {
 	font-size: 1.2rem;
 	font-weight: bold;
 
+	text-align: right;
+}
+
+#total-container {
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+}
+
+#total {
+	border-top: 1px solid gray;
+}
+
+#total .item {
+	font-weight: bold;
 	text-align: right;
 }
 </style>
